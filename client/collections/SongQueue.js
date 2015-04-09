e774d840269f3a29cac7f0a3d6fed4e79daec2a5
jpiv <jjpizzo4@gmail.com>
2015-04-08 20:04:26 -0700

    Finished through add

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,11 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', function() {
+        if (this.length === 1) {
+          this.playFirst();
+        }
+      },this);
 
   },
 
